6c789b2767868209286235d34afb23fe6ab63edc
Lindemberg <lindembergncastro@outlook.com>
2020-06-10 15:12:51 -0300

    injecao  de dependencia com tsyringer

--- server/src/modules/appointments/services/CreateAppointmentService.ts
+++ server/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,5 +1,5 @@
 import { startOfHour } from 'date-fns';
-
+import { injectable, inject } from 'tsyringe';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import AppError from '@shared/errors/AppError';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
@@ -9,8 +9,12 @@ interface IRequest {
   date: Date;
 }
 // regras de negócio
+@injectable()
 class CreateAppointmentService {
-  constructor(private appointmentsRepository: IAppointmentsRepository){}
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository
+    ){}
 
   public async execute({ provider_id, date }: IRequest): Promise<Appointment> {
 
