0a7591e5738e87ae15d337b093cc71f8b49e6c4a
Lindemberg <lindembergncastro@outlook.com>
2020-06-15 11:50:11 -0300

    many changes

--- server/src/shared/container/index.ts
+++ server/src/shared/container/index.ts
@@ -1,18 +1,22 @@
 import { container } from 'tsyringe';
 
-import IAppointmentsRepository from  '@modules/appointments/repositories/IAppointmentsRepository';
+import '@modules/users/providers';
+import './providers';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
-import IUsersRepository from  '@modules/users/repositories/IUsersRepository';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+
+// import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+// import User from '@modules/users/infra/typeorm/repositories/User';
 
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
-   AppointmentsRepository
+  AppointmentsRepository,
 );
-
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
-   UsersRepository
+  UsersRepository,
 );
